e0a31dfec0aa6c0e4e074c09d8069d8b90bedfe6
Kenny Marsh <kenny.m@kaholo.io>
2023-10-24 13:29:45 +0100

    adjust for new tenant scripts in helm chart

--- roles/kaholohelm/scripts/kaholo-backup.sh
+++ roles/kaholohelm/scripts/kaholo-backup.sh
@@ -3,4 +3,4 @@ MK8S_NAMESPACE={{ mk8s_namespace }}
 BAKDIR258=$MK8S_NAMESPACE-backup-$(date -I)
 mkdir $BAKDIR258
 cd kaholo-helm/scripts/tenants-management
-./TENANT_DUMP.sh $MK8S_NAMESPACE ~/$BAKDIR258
+./TENANT_DUMP.sh -n $MK8S_NAMESPACE -p ~/$BAKDIR258
